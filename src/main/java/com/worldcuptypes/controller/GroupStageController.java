4be792301beeb323302235110e00c412fca8c63f
fureq <furmanszymon@gmail.com>
2018-06-06 22:58:33 +0200

    Rename Controller

--- src/main/java/com/worldcuptypes/controller/GroupStageController.java
+++ src/main/java/com/worldcuptypes/controller/GroupStageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-public class ResourcesController {
+public class GroupStageController {
 
     private final ResourcesService resourcesService;
 
